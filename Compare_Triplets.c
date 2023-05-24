c3e01a51776fb9e4fd0e159d529dc8cf716c7dec
roopeshshyam <121609650+roopeshshyam@users.noreply.github.com>
2023-05-24 12:07:01 +0530

    #include<stdio.h> int main() {     int a[3],b[3],c=0,s=0,i;     scanf("%d %d %d",&a[0],&a[1],&a[2]);     scanf("%d %d %d",&b[0],&b[1],&b[2]);     for(i=0;i<3;i++)     {         if(a[i]>b[i])           c++;         else if(b[i]>a[i])           s++;     }     printf("%d %d",c,s); }

---
+++ Compare_Triplets.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+    int a[3],b[3],c=0,s=0,i;
+    scanf("%d %d %d",&a[0],&a[1],&a[2]);
+    scanf("%d %d %d",&b[0],&b[1],&b[2]);
+    for(i=0;i<3;i++)
+    {
+        if(a[i]>b[i])
+          c++;
+        else if(b[i]>a[i])
+          s++;
+    }
+    printf("%d %d",c,s);
+}
